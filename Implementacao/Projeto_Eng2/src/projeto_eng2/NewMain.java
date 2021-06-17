3486eaa9cd203ef4b6c9111facaf3a6c3109606f
FlavioBarbosa2304 <66962670+FlavioBarbosa2304@users.noreply.github.com>
2021-06-17 18:35:17 -0300

    Subindo a implementação
    Subindo os arquivos referentes a implementação

---
+++ Implementacao/Projeto_Eng2/src/projeto_eng2/NewMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_eng2;
+
+/**
+ *
+ * @author flavi
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaLogin janela1 = new TelaLogin();
+        janela1.setVisible(true);
+    }
+    
+}
